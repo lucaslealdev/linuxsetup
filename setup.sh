c1809fd97aaf3e6c7948116f80007123bc183f39
Lucas Leal <5296637+lucaslealdev@users.noreply.github.com>
2023-06-30 15:29:48 -0300

    teste inicial de sh

---
+++ setup.sh
@@ -0,0 +1,63 @@
+set -e
+cat << EOF
+===========================
+===========================
+=== Atualizando pacotes ===
+===========================
+===========================
+EOF
+sudo apt update
+cat << EOF
+===========================
+===========================
+= Instalando dependências =
+===========================
+===========================
+EOF
+sudo apt install software-properties-common apt-transport-https wget -y
+cat << EOF
+===========================
+===========================
+===== Instalando ZSH ======
+===========================
+===========================
+EOF
+sudo apt install zsh -y
+source ~/.bashrc
+sudo -u $SUDO_USER chsh -s $(which zsh)
+cat << EOF
+===========================
+===========================
+==== Instalando OMZSH =====
+===========================
+===========================
+EOF
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cat << EOF
+===========================
+===========================
+===== Instalando NVM ======
+===========================
+===========================
+EOF
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm install --lts
+nvm use --lts
+cat << EOF
+===========================
+===========================
+==== Instalando vsCode ====
+===========================
+===========================
+EOF
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code -y
+cat << EOF
+===========================
+===========================
+== Instalação finalizada ==
+======= Bora codar ! ======
+===========================
+EOF
