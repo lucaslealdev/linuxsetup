a087bfa54ec47857fedcfafe22f1d3f8fb6c1153
Lucas Leal <5296637+lucaslealdev@users.noreply.github.com>
2023-07-03 14:27:52 -0300

    reload nvm vars

--- setup.sh
+++ setup.sh
@@ -40,7 +40,9 @@ cat << EOF
 ===========================
 EOF
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install --lts
 nvm use --lts
 cat << EOF
